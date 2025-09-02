b2f57d8096ec4e5ccd4625e82ce6fc0482f2ec4e
Lucas Garcia e Silva <llucasgarciaesilva@hotmail.com>
2025-09-01 21:39:22 -0300

    preparando os dados do form para serem enviados à API

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -14,6 +14,7 @@ divInputDate.addEventListener("click", () => {
 });
 
 const selectedDate = document.getElementById("date");
+const clientName = document.getElementById("client");
 
 // Define a data atual
 const inputToday = dayjs(new Date()).format("YYYY-MM-DD");
@@ -27,4 +28,36 @@ selectedDate.min = inputToday;
 form.onsubmit = (event) => {
   // Previne o comportamento padrão de carregar a página.
   event.preventDefault();
+
+  try {
+    // Recuperando o nome do cliente
+    const name = clientName.value.trim();
+    if (!name) {
+      return alert("Informe o nome do cliente");
+    }
+
+    // Recuperando o horário selecionado
+    const hourSelected = document.querySelector(".hour-selected");
+    if (!hourSelected) {
+      return alert("Selecione um horário");
+    }
+
+    //Recuperando apenas a hora do horário
+    const [hour] = hourSelected.innerText.split(":");
+
+    //Inserindo a hora na data
+    const when = dayjs(selectedDate.value).add(hour, "hour");
+
+    //Gera um ID para termos um identificador de cada submit de agendamento
+    const id = new Date().getTime();
+
+    console.log({
+      id,
+      name,
+      when,
+    });
+  } catch (error) {
+    alert("Não foi possível realizar o agendamento");
+    console.log(error);
+  }
 };
