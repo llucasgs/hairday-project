a29166c3318fba3807285bf90baa37c881a3cbe9
Lucas Garcia e Silva <llucasgarciaesilva@hotmail.com>
2025-09-03 23:46:47 -0300

    criando o serviço para buscar agendamentos na api de acordo com o dia selecionado

--- src/services/schedule-new.js
+++ src/services/schedule-new.js
@@ -2,11 +2,12 @@ import { apiConfig } from "./api-config.js";
 
 export async function scheduleNew({ id, name, when }) {
   try {
+    // Faz a requisição para enviar os dados do agendamento.
     await fetch(`${apiConfig.baseURL}/schedules`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        // Informa ao servidor que o corpo da requisição estará no formato JSON
+        // Informa ao servidor que o corpo da requisição estará no formato JSON.
       },
       body: JSON.stringify({ id, name, when }),
       /* Converte o objeto JS em uma string JSON (serialização: transformar estrutura de dados em string para enviar pela rede).
